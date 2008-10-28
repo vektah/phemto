9c932eac667f1ae7b725100ce3276a95bec10149
lastcraft <lastcraft@689550c6-99b5-42a6-b8d6-a86e191e286b>
2008-10-28 00:54:32 +0000

    Filling out acceptance tests
    git-svn-id: https://phemto.svn.sourceforge.net/svnroot/phemto/trunk@9 689550c6-99b5-42a6-b8d6-a86e191e286b

--- acceptance_tests.php
+++ acceptance_tests.php
@@ -34,25 +34,99 @@ class CanAutomaticallyInstantiateKnownInterfaces extends UnitTestCase {
 	
 	function testCanBeConfiguredToPreferSpecificImplementation() {
 		$injector = new Phemto();
-		$injector->prefer('SecondImplementation');
+		$injector->willUse('SecondImplementation');
 		$this->assertIsA($injector->instantiate('InterfaceWithManyImplementations'),
 						 'SecondImplementation');
 	}
 }
 
+interface Hinted { }
+
+class HintedConstructor implements Hinted {
+	function __construct(InterfaceWithOneImplementation $one) {
+		$this->args = func_get_args();
+	}
+}
+
+class HintedConstructorWithDependencyChoice implements Hinted {
+	function __construct(InterfaceWithManyImplementations $alternate) {
+		$this->args = func_get_args();
+	}
+}
+
+class RepeatedHintConstructor {
+	function __construct(InterfaceWithManyImplementations $first, InterfaceWithManyImplementations $second) {
+		$this->args = func_get_args();
+	}
+}
+
 class CanAutomaticallyInjectTypeHintedDependencies extends UnitTestCase {
+	function testSimpleDependenciesAreFulfilledAutomatically() {
+		$injector = new Phemto();
+		$this->assertIdentical($injector->instantiate('HintedConstructor'),
+							   new HintedConstructor(new OnlyImplementation()));
+	}
+	
+	function testRepeatedHintJustGetsTwoSeparateInstances() {
+		$injector = new Phemto();
+		$injector->willUse('SecondImplementation');
+		$this->assertEqual(
+				$injector->instantiate('RepeatedHintConstructor'),
+				new RepeatedHintConstructor(new SecondImplementation(), new SecondImplementation()));
+	}
 }
 
 class CanInjectDependenciesByVariableName extends UnitTestCase {
+	function testExplicitlyNamedVariables() {
+		$injector = new Phemto();
+		$injector->forVariable('first')->willUse('FirstImplementation');
+		$injector->forVariable('second')->willUse('SecondImplementation');
+		$this->assertEqual(
+				$injector->instantiate('RepeatedHintConstructor'),
+				new RepeatedHintConstructor(new FirstImplementation(), new SecondImplementation()));
+	}
 }
 
 class CanUseDifferentDependencySetWithinAnInterface extends UnitTestCase {
+	function testCanOverridePreferenceWhenInstantiatingSpecificInstance() {
+		$injector = new Phemto();
+		$injector->willUse('FirstImplementation');
+		$injector->whenCreating('Hinted')->willUse('SecondImplementation');
+		$this->assertEqual(
+				$injector->instantiate('HintedConstructorWithDependencyChoice'),
+				new HintedConstructorWithDependencyChoice(new SecondImplementation()));
+	}
 }
 
 class CanInstantiateObjectsAsSingletons extends UnitTestCase {
+	function testSameInstanceIsReusedForSingleton() {
+		$injector = new Phemto();
+		$injector->willUse(new Singleton('LoneClass'));
+		$this->assertSame(
+				$injector->instantiate('LoneClass'),
+				$injector->instantiate('LoneClass'));
+	}
 }
 
 class CanInstantiateFromSessions extends UnitTestCase {
+	function tearDown() {
+		$_SESSION['slot'] = false;
+	}
+	
+	function testSessionableInstanceWrittenToSession() {
+		$injector = new Phemto();
+		$injector->willUse(new Sessionable('slot', 'LoneClass'));
+		$_SESSION['slot'] = false;
+		$lone = $injector->instantiate('LoneClass');
+		$this->assertSame($lone, $_SESSION['slot']);
+	}
+	
+	function testSessionableInstancePulledFromSessionIfExists() {
+		$_SESSION['slot'] = new LoneClass();
+		$injector = new Phemto();
+		$injector->willUse(new Sessionable('slot', 'LoneClass'));
+		$this->assertSame($injector->instantiate('LoneClass'), $_SESSION['slot']);
+	}
 }
 
 class CanCallSettersToCompleteInitialisation extends UnitTestCase {
@@ -61,6 +135,6 @@ class CanCallSettersToCompleteInitialisation extends UnitTestCase {
 class WorksWithNamespaces extends UnitTestCase {
 }
 
-class WorksWithAutoload extends UnitTestCase {
+class AsMuchAsPossibleWorksWithAutoload extends UnitTestCase {
 }
 ?>
