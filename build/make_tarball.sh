a9e2eb59b8a5e631030231d54003a18ea327f169
mgruff <mgruff@689550c6-99b5-42a6-b8d6-a86e191e286b>
2009-04-30 16:50:36 +0000

    Updating build script removing some filesystem assumptions which prevented the script from running on my own machine. Also, phemto/VERSION is now tagged with the subversion revision number (after building a release - doesn't affect release naming). This might be useful for support queries.
    git-svn-id: https://phemto.svn.sourceforge.net/svnroot/phemto/trunk@66 689550c6-99b5-42a6-b8d6-a86e191e286b

--- build/make_tarball.sh
+++ build/make_tarball.sh
@@ -1,13 +1,70 @@
-cd ../..
-NAME=phemto_`cat phemto/VERSION`.tar.gz
-xsltproc phemto/docs/xslt/bundled.xslt phemto/docs/xml/index.xml > phemto/index.html
-FILES=(phemto/README \
-          phemto/LICENCE \
-          phemto/index.html \
-          phemto/docs/bundled.css \
-          phemto/phemto.php \
-          phemto/lifecycle.php \
-          phemto/repository.php \
-          phemto/LICENCE \
-          phemto/VERSION)
-tar -zcf $NAME ${FILES[*]}
+#!/bin/bash
+
+# absolute path to parent of "build/"
+# $1 - this script's $0 arg
+getTrunk()
+{
+    cd `dirname $1`/
+    cd ..
+    echo `pwd -P`
+}
+
+createTemporaryBuild()
+{
+    local FILE
+    local FILES=(
+        README \
+        LICENCE \
+        phemto.php \
+        lifecycle.php \
+        repository.php \
+        LICENCE \
+        VERSION        
+        )
+
+    mkdir -p ${TMP_BUILD_DIR}'phemto'
+
+    for FILE in ${FILES[@]}
+    do
+        cp ${TRUNK}${FILE} ${TMP_BUILD_DIR}'phemto/'
+    done
+
+    makeDocs
+    echo "revision: `getCurrentRevision $TRUNK`" >> ${TMP_BUILD_DIR}'phemto/VERSION'
+}
+
+makeDocs()
+{
+    mkdir -p ${TMP_BUILD_DIR}'phemto/docs'
+    cp ${TRUNK}'docs/bundled.css' ${TMP_BUILD_DIR}'phemto/docs/'
+    xsltproc ${TRUNK}'docs/xslt/bundled.xslt' ${TRUNK}'docs/xml/index.xml' > ${TMP_BUILD_DIR}'phemto/index.html'
+}
+
+getCurrentRevision()
+{
+    echo `svn info $TRUNK | grep Revision:[[:space:]]*[[:digit:]]*$ | grep -o [[:digit:]]*$`
+}
+
+createReleaseFile()
+{
+    cd $TMP_BUILD_DIR
+    tar -zcf $NAME *
+    mv $NAME $WRITABLE_DIR
+    echo "release file is: "${WRITABLE_DIR}${NAME} 
+}
+
+cleanUp()
+{
+    rm -rf $TMP_BUILD_DIR
+}
+
+#-----------------------------------------------------
+
+WRITABLE_DIR=/tmp/
+TMP_BUILD_DIR=${WRITABLE_DIR}'phemto-tmp-build/'
+TRUNK=`getTrunk $0`/
+NAME=phemto_`cat ${TRUNK}'VERSION'`.tar.gz
+createTemporaryBuild
+createReleaseFile
+cleanUp
+exit 0
