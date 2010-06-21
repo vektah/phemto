e28d0f5df15220e47d422249836fb234720e137b
lastcraft <lastcraft@689550c6-99b5-42a6-b8d6-a86e191e286b>
2010-06-21 19:21:22 +0000

    Tracking down session bug
    git-svn-id: https://phemto.svn.sourceforge.net/svnroot/phemto/trunk@92 689550c6-99b5-42a6-b8d6-a86e191e286b

--- acceptance/can_instantiate_from_session.php
+++ acceptance/can_instantiate_from_session.php
@@ -4,17 +4,30 @@ require_once(dirname(__FILE__) . '/../phemto.php');
 
 class SerialiseMe { }
 
+class HoldsSessionable {
+    public $dependency;
+    
+    function __construct(SerialiseMe $dependency) {
+        $this->dependency = $dependency;
+    }
+}
+
 class CanInstantiateFromSessions extends UnitTestCase {
     function tearDown() {
         $_SESSION['slot'] = false;
+        $_SESSION['SerialiseMe'] = false;
     }
 
     function testSessionableInstanceWrittenToSession() {
         $injector = new Phemto();
         $injector->willUse(new Sessionable('SerialiseMe'));
-        $_SESSION['SerialiseMe'] = false;
-        $lone = $injector->create('SerialiseMe');
-        $this->assertSame($lone, $_SESSION['SerialiseMe']);
+        $this->assertSame($injector->create('SerialiseMe'), $_SESSION['SerialiseMe']);
+    }
+
+    function testCanOverideSessionSlot() {
+        $injector = new Phemto();
+        $injector->willUse(new Sessionable('SerialiseMe', 'slot'));
+        $this->assertSame($injector->create('SerialiseMe'), $_SESSION['slot']);
     }
 
     function testSessionableInstancePulledFromSessionIfExists() {
@@ -22,6 +35,19 @@ class CanInstantiateFromSessions extends UnitTestCase {
         $injector = new Phemto();
         $injector->willUse(new Sessionable('SerialiseMe', 'slot'));
         $this->assertSame($injector->create('SerialiseMe'), $_SESSION['slot']);
+    }
+    
+    function testSameInstanceFromSessionWithinSameProcess() {
+        $injector = new Phemto();
+        $injector->willUse(new Sessionable('SerialiseMe'));
+        $this->assertSame($injector->create('SerialiseMe'), $injector->create('SerialiseMe'));
+    }
+    
+    function TODO_testSessionableWorksWithinContext() {
+        $injector = new Phemto();
+        $injector->whenCreating('HoldsSessionable')->willUse(new Sessionable('SerialiseMe'));
+        $holder = $injector->create('HoldsSessionable');
+        $this->assertSame($holder->dependency, $_SESSION['SerialiseMe']);
     }
 }
 ?>
